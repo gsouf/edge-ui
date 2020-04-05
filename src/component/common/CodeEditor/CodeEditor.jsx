04b2b598cb3c686fb7bfdf47b7779dd1643bf043
sghzal <sghzal@gmail.com>
2020-04-05 19:13:20 +0200

    improved monaco integration

--- src/component/common/CodeEditor/CodeEditor.jsx
+++ src/component/common/CodeEditor/CodeEditor.jsx
@@ -4,10 +4,48 @@ import PropTypes from 'prop-types';
 import { monaco, ControlledEditor } from '@monaco-editor/react';
 
 monaco.config({
-  urls: {
-    monacoLoader: '/monaco-editor/loader.js',
-    monacoBase: '/monaco-editor',
+  paths: {
+    vs: '/vs',
   },
+});
+
+monaco.init().then((monaco) => {
+  // register edgeql syntax
+  monaco.languages.register({ id: 'edgeql' });
+  // set completion
+  monaco.languages.registerCompletionItemProvider('edgeql', {
+    provideCompletionItems: function (model, position) {
+      // TODO make a real completion that understands the code
+      const keywords = [
+        'SELECT',
+        'FILTER',
+        'LIMIT',
+        'UNION',
+        'INSERT',
+        'UPDATE',
+        'SET',
+        'WITH',
+        'EXITS',
+        'IN',
+        'IS',
+        'DELETE',
+        'CREATE',
+        'FUNCTION',
+        'USING',
+        'abstract',
+        'type',
+        'extending',
+      ];
+      const suggestions = keywords.map((k) => ({
+        label: k,
+        kind: monaco.languages.CompletionItemKind.Text,
+        insertText: `${k} `,
+        insertTextRules:
+          monaco.languages.CompletionItemInsertTextRule.KeepWhitespace,
+      }));
+      return { suggestions };
+    },
+  });
 });
 
 const useStyles = makeStyles((theme) =>
@@ -18,27 +56,39 @@ const useStyles = makeStyles((theme) =>
 
 export default function CodeEditor(props) {
   const classes = useStyles();
-  const [text, setText] = useState('default');
 
   return (
     <div className={classes.root}>
       <ControlledEditor
-        language="javascript"
+        language="edgeql"
         height="15rem"
         width="100%"
-        theme="dark"
+        theme="vs-dark"
         options={{
           minimap: {
             enabled: false,
           },
         }}
-        value={text}
+        value={props.text}
         onChange={(ev, value) => {
-          setText(value);
+          props.setText(value);
+        }}
+        editorDidMount={(_, editor) => {
+          editor.onKeyDown((e) => {
+            if (props.requestSubmit && e.ctrlKey && e.keyCode === 3) {
+              e.preventDefault();
+              e.stopPropagation();
+              props.requestSubmit(editor.getValue());
+            }
+          });
         }}
       />
     </div>
   );
 }
 
-CodeEditor.propTypes = {};
+CodeEditor.propTypes = {
+  text: PropTypes.string.isRequired,
+  setText: PropTypes.func.isRequired,
+  requestSubmit: PropTypes.func,
+};
--- src/component/common/CodeEditor/CodeEditor.stories.js
+++ src/component/common/CodeEditor/CodeEditor.stories.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CodeEditor from './CodeEditor';
+import { action } from '@storybook/addon-actions';
 
 export default {
-  title: 'Code Editor',
+  title: 'Query / Code Editor',
   component: CodeEditor,
   excludeStories: /.*Data$/,
 };
 
+const ControlledCodeEditor = () => {
+  const [text, setText] = useState('default');
+
+  return (
+    <CodeEditor
+      text={text}
+      setText={(newText) => {
+        action('setText')(newText);
+        setText(newText);
+      }}
+      requestSubmit={(txt) => {
+        action('requestSubmit')(txt);
+      }}
+    />
+  );
+};
+
 export const Default = () => (
   <div style={{ margin: '3rem' }}>
-    <CodeEditor />
+    <ControlledCodeEditor />
   </div>
 );
